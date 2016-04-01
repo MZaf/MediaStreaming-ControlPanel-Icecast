ab318edf449b0964bca3f3cbad425c966a3fd01e
MZaf <acer.bwp@gmail.com>
2016-04-01 14:44:00 +0500

    Initial commit

---
+++ application/config/icegenerator.php
@@ -0,0 +1,13 @@
+<?php
+
+$config["icegen_ip"] = "server_ip";
+$config["icegen_port"] = "icecast_port";
+$config["icegen_mount"] = "/radio";
+$config["icegen_user"] = "source";
+$config["icegen_password"] = "icecast password";
+$config["icegen_format"] = "1";
+$config["icegen_type"] = "m3u:";
+$config["icegen_name"] = "Your Radio Name";
+$config["icegen_genre"] = "Only good music";
+$config["icegen_description"] = "Your radio description";
+$config["icegen_timezone"] = "Europe/Rome";
